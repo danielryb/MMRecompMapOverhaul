8e8156929823260fb7787ae12ad1f909530010e1
danielryb <59661841+danielryb@users.noreply.github.com>
2025-01-21 01:07:37 +0100

    add support for custom map positions

---
+++ src/constants.h
@@ -0,0 +1,76 @@
+#ifndef __CONSTANTS_H__
+#define __CONSTANTS_H__
+
+// x positions
+#define GREAT_BAY_X         65
+#define ZORA_HALL_X         87
+#define ROMANI_RANCH_X      132
+#define DEKU_PALACE_X       143
+#define WOODFALL_X          165
+#define CLOCK_TOWN_X        156
+#define SNOWHEAD_X          157
+#define IKANA_GRAVEYARD_X   191
+#define IKANA_CANYON_X      215
+#define GORON_VILLAGE_X     204
+#define STONE_TOWER_X       236
+
+#define OWL_WARP_GREAT_BAY_COAST_X  GREAT_BAY_X
+#define OWL_WARP_ZORA_CAPE_X        ZORA_HALL_X
+#define OWL_WARP_SNOWHEAD_X         SNOWHEAD_X
+#define OWL_WARP_MOUNTAIN_VILLAGE_X GORON_VILLAGE_X
+#define OWL_WARP_CLOCK_TOWN_X       CLOCK_TOWN_X
+#define OWL_WARP_MILK_ROAD_X        ROMANI_RANCH_X
+#define OWL_WARP_WOODFALL_X         WOODFALL_X
+#define OWL_WARP_SOUTHERN_SWAMP_X   DEKU_PALACE_X
+#define OWL_WARP_IKANA_CANYON_X     IKANA_CANYON_X
+#define OWL_WARP_STONE_TOWER_X      STONE_TOWER_X
+
+// y positions
+#define GREAT_BAY_Y         113
+#define ZORA_HALL_Y         144
+#define ROMANI_RANCH_Y      134
+#define DEKU_PALACE_Y       170
+#define WOODFALL_Y          153
+#define CLOCK_TOWN_Y        123
+#define SNOWHEAD_Y          77
+#define IKANA_GRAVEYARD_Y   112
+#define IKANA_CANYON_Y      117
+#define GORON_VILLAGE_Y     73
+#define STONE_TOWER_Y       125
+
+#define OWL_WARP_GREAT_BAY_COAST_Y  GREAT_BAY_Y
+#define OWL_WARP_ZORA_CAPE_Y        ZORA_HALL_Y
+#define OWL_WARP_SNOWHEAD_Y         SNOWHEAD_Y
+#define OWL_WARP_MOUNTAIN_VILLAGE_Y GORON_VILLAGE_Y
+#define OWL_WARP_CLOCK_TOWN_Y       CLOCK_TOWN_Y
+#define OWL_WARP_MILK_ROAD_Y        ROMANI_RANCH_Y
+#define OWL_WARP_WOODFALL_Y         WOODFALL_Y
+#define OWL_WARP_SOUTHERN_SWAMP_Y   DEKU_PALACE_Y
+#define OWL_WARP_IKANA_CANYON_Y     IKANA_CANYON_Y
+#define OWL_WARP_STONE_TOWER_Y      STONE_TOWER_Y
+
+// float positions' constants
+#define SCALE_X (207.0f / 263.0f)
+#define SCALE_Y -SCALE_X
+
+#define OFFSET_X 4.0f
+#define OFFSET_Y -OFFSET_X
+
+#define FLOAT_TO_DOT_X 1.275f
+#define FLOAT_TO_DOT_Y FLOAT_TO_DOT_X
+
+// game objects
+#define FACE_WIDTH 16
+#define FACE_HEIGHT FACE_WIDTH
+
+#define DOT_WIDTH 8
+#define DOT_HEIGHT DOT_WIDTH
+
+#define CURSOR_CIRCLE_WIDTH 8
+#define CURSOR_CIRCLE_HEIGHT CURSOR_CIRCLE_WIDTH
+#define CURSOR_RADIUS 6
+
+#define OWL_WIDTH 24
+#define OWL_HEIGHT 12
+
+#endif /* __CONSTANTS_H__ */
